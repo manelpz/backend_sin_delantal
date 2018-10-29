f873bb7ecbeb9be4b704e6b907cc180dd91fb834
Emmanuel López Guerrero <manelpz@icloud.com>
2018-10-29 15:37:18 -0600

    Add foreign keys

---
+++ server/migrations/20181029213236-add-foreign-keys.js
@@ -0,0 +1,76 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn(
+      "Addresses_Users",
+      "user_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Users',
+          key:"id"
+        }
+      }
+    )
+
+    queryInterface.addColumn(
+      "Addresses_Restaurants",
+      "restaurant_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Restaurants',
+          key:"id"
+        }
+      }
+    )
+
+    queryInterface.addColumn(
+      "Comments",
+      "order_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Orders',
+          key:"id"
+        }
+      }
+    )
+
+    queryInterface.addColumn(
+      "Dishes",
+      "restaurant_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Restaurants',
+          key:"id"
+        }
+      }
+    )
+    queryInterface.addColumn(
+      "Orders",
+      "dish_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Dishes',
+          key:"id"
+        }
+      },
+      "user_id",{
+        type:Sequelize.UUID,
+        references:{
+          model:'Users',
+          key:"id"
+        }
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
